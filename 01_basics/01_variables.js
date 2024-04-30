4b35c910a4e65322e356bfeeac60f93baaa27e5c
gauravkumar71 <168508421+gauravkumar71@users.noreply.github.com>
2024-04-30 06:10:35 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,3 +3,7 @@ let studentEmail = "gauravkumar32335@gmail.com"
 studentCity = "Raxaul"
 
 console.log("studentEmail");
+
+studentEmail = "apnagaurav01@gmail.com"
+studentCity = "Patna"
+console.table({studentEmail,studentCity})
