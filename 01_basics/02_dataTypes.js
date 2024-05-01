73f918b2416b55ad6cf00411541f89124aedb806
gauravkumar71 <168508421+gauravkumar71@users.noreply.github.com>
2024-05-01 05:24:56 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all jS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+let name = "hitesh"
+let age = 13;
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined=>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
+
+
