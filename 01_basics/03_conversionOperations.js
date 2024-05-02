754c240780f64417c1dec13a8c708b5da907e5c0
gauravkumar71 <168508421+gauravkumar71@users.noreply.github.com>
2024-05-02 17:05:23 +0000

    conversion done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33" => 33
@@ -14,14 +14,25 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1;
 
-let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
 
 // 1 => ture; 0 => false
 // " "  => false
 // "hitesh" => true
 
 let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+console.log(+true);
+console.log(+ " ");
+
+let num1, num2, num3
+
+num = num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
