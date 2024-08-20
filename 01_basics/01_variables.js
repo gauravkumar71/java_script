2e210fd5fd242ddfe5b7c7249e3a690d1e104a72
gauravkumar71 <168508421+gauravkumar71@users.noreply.github.com>
2024-08-20 08:23:02 +0000

    01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,7 +3,10 @@ let studentEmail = "gauravkumar32335@gmail.com"
 studentCity = "Raxaul"
 
 console.log("studentEmail");
-
+/*
+  Prefer not to use var
+  because of issue in block scope and functional scope
+*/
 studentEmail = "apnagaurav01@gmail.com"
 studentCity = "Patna"
 console.table({studentEmail,studentCity})
