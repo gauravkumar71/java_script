4091aa73f968c1ff0f1c04f7b63547893078b783
gauravkumar71 <168508421+gauravkumar71@users.noreply.github.com>
2024-08-26 17:25:32 +0000

    01_function.js

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -33,8 +33,28 @@ function sayMyName(){
       return '$ {username} just logged in'
    }
    // console.log(loginUserMessage("hitesh"))
-   console.log(loginUserMessage("gaurav"))
+  //  console.log(loginUserMessage("gaurav"))
 
 
-   
+  function calculateCartPrice( val1, val2,...num1){
+       return num1
+  }
 
+
+  //  console.log(calculateCartPrice(200, 400, 500, 2000))
+
+  const user = {
+    username: "hitesh",
+    price: 199
+  }
+
+  export function handleObject(anyobject){
+       console.log('username is ${anyobject.username} and price is ${anyobject.price}');
+  }
+
+ const myNewArray = [200,400,100,600]
+ function returnSecondValue(getArray){
+     return getArray[1]
+ }
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,100]));
---
+++ 03_basics/newFile.js
@@ -0,0 +1,7 @@
+const { handleObject } = require("./01_function");
+
+// handleObject(user)
+handleObject({
+  username: "sam",
+  price: 399
+});
