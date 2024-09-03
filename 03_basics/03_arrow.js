7f73c00033e4d94f7a113f330d8b07f5df0c84d1
gauravkumar71 <168508421+gauravkumar71@users.noreply.github.com>
2024-09-03 13:30:34 +0000

    03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -14,6 +14,40 @@ const user = {
 
 // console.log(this);
 
-function chai(){
+// This keyword doesnot work in the function
+// Browser me global variable window hota hai
+
+// function chai(){
+//      let username = "hitesh"
+//      console.log(this);
+// }
+
+// chai()
+
+const chai = () => {
+     let username = "hitesh"
      console.log(this);
 }
+
+// chai()
+
+// Basic Arrow Function
+// Explicit  function  me return likna padta hai
+
+// const addTwo = (num1,num2) =>
+// {
+//      return num1+num2
+// }
+
+// Implicit Return
+// curly bracesis me return nahi likha jata h
+
+// const addTwo = (num1,num2) =>  (num1+num2)
+
+// object wrap
+
+const addTwo = (num1,num2) =>  ({username:"hitesh"})
+
+     
+     
+console.log(addTwo(3, 4))
