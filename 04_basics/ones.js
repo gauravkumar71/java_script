25d2bae72d06da3af8fe88604e35ec168ab7d13d
gauravkumar71 <168508421+gauravkumar71@users.noreply.github.com>
2024-09-07 08:22:49 +0000

    controlflow

---
+++ 04_basics/ones.js
@@ -0,0 +1,91 @@
+
+// const tempreature = 41;
+// if(tempreature ===40)
+// {
+//    console.log("less than 50");
+// }
+//  else
+// {
+//   console.log("Tempreature is grater than 50");
+// }
+//   console.log("Excute");
+
+
+// scope check
+
+// const score = 200;
+
+// if(score>100)
+// {
+//     const power = "fly";
+//     console.log(`User power: ${power}`);
+// }
+//   console.log(`User power: ${power}`);
+
+
+// Short Hand notation for if condition
+
+  const balance = 1000;
+
+  // if(balance > 500) console.log("test"),
+  // console.log("test2");
+
+  // Nested loops
+
+  // if(balance < 500)
+  // {
+  //   console.log("Less than 500");
+  // }
+  // else if(balance < 750)
+  // {
+  //     console.log("Less than 750");
+  // }
+  // else
+  // {
+  //   console.log("Less than 1200");
+  // }
+
+//   const userLoggedIn = true;
+//   const debitCard = true;
+//   const LoggedInFromGoogle = false;
+//   const LoggedInFromEmail = true;
+
+// if(userLoggedIn && debitCard)
+// {
+//   console.log("shopping")
+// }
+
+// if(LoggedInFromGoogle || LoggedInFromEmail)
+// {
+//   console.log("allow")
+// }
+
+
+// falsey Value
+
+//  false, 0, -0, BigInt 0n, null, undefined, NaN
+
+
+// truthy values
+
+// "0", "false", " ", [], {}, function(){}
+
+
+// How to check object in if condition
+
+const emptyObject = {}
+
+// if(Object.keys(emptyObject).length===0){
+//   console.log("Object is empty");
+// }
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100;
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80")
+
+
+  
+ 
